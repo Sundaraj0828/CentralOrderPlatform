53f76f9ca90745c638bfb2ac8dc9ea71af72aeb1
LCSundaraj <77037583+Sundaraj0828@users.noreply.github.com>
2026-04-19 13:34:43 +0530

    Update mail_msg_config_db.py

--- mail_msg_config_db.py
+++ mail_msg_config_db.py
@@ -12,7 +12,7 @@ def connect_db():
   global con
   global db
   global col
-  con = MongoClient('mongodb+srv://Sourav:l9EG8ULwylphgjHS@nexusfieldservice.axvp7.mongodb.net/Properties?retryWrites=true&w=majority')
+  con = MongoClient('')
   db = con.Properties
   col = db.sensitive_access
 
